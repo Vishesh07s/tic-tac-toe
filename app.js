1b7a20a90ccb6c99a2b9a60eede19e4f6b849f69
Utkarsh patel <utkarshishu2627@gmail.com>
2025-07-10 02:09:44 +0530

    made it in so single and double players can play. if case of single player the system will play against you. and made the css much better

--- app.js
+++ app.js
@@ -1,85 +1,156 @@
-let boxes =  document.querySelectorAll(".box");
-let resetBtn = document.querySelector("#reset");
-let newGameBtn = document.querySelector("#newbtn")
-let msgcontainer = document.querySelector(".msg-container");
-let msg = document.querySelector("#msg");
+var onePlayerBtn = document.getElementById('onePlayer');
+var twoPlayerBtn = document.getElementById('twoPlayer');
+var menu = document.getElementById('menu');
+var game = document.getElementById('game');
+var boxes = document.querySelectorAll('.box');
+var resetBtn = document.getElementById('resetBtn');
+var menuBtn = document.getElementById('menuBtn');
+var turnText = document.querySelector('.turn');
+var winnerMessage = document.querySelector('.winner-message');
+var message = document.getElementById('message');
+var playAgainBtn = document.getElementById('playAgain');
 
-let turnO =  true;        //player x,player o 
+var gameMode = 'singlePlayer';
+var currentPlayer = 'X';
+var board = ["", "", "", "", "", "", "", "", ""];
+var isGameActive = false;
 
-const winPatterns = [
-  [0,1,2],
-  [0,3,6],
-  [0,4,8],
-  [1,4,7],
-  [2,5,8],
-  [2,4,6],
-  [3,4,5],
-  [6,7,8],
+var winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ];
 
-const resetgame = () => {
-  turnO = true;
-  enableboxes();
-  msgcontainer.classList.add("hide")
-
-}
-
-boxes.forEach((box) =>{
-  box.addEventListener("click",() =>{
-    console.log("box was clicked");
-    if(turnO){
-      box.innerText = "O"
-      box.classList.add("o");
-      turnO = false;
-    }
-    else{
-      box.innerText = "X";
-      box.classList.add("x");
-      turnO = true;
-    }
-    box.disabled = true;
-
-    checkWinner()
-  })
-});
-
-
-
-const disabledboxes = () => {
-  for(let box of boxes){
-    box.disabled = true ;
-  }
-};
-const enableboxes = () => {
-  for(let box of boxes){
-    box.disabled = false ;
-    box.innerText = "";
-  }
+onePlayerBtn.onclick = function() {
+    gameMode = 'singlePlayer';
+    startGame();
 };
 
-const showWinner = (winner) => {
-  msg.innerText = `Congratulation, Winner is ${winner}`;
-  msgcontainer.classList.remove("hide");
+twoPlayerBtn.onclick = function() {
+    gameMode = 'twoPlayer';
+    startGame();
+};
+
+menuBtn.onclick = function() {
+    game.style.display = 'none';
+    menu.style.display = 'flex';
+};
+
+resetBtn.onclick = function() {
+    startNewGame();
+};
+
+playAgainBtn.onclick = function() {
+    startNewGame();
+};
+
+function startGame() {
+    menu.style.display = 'none';
+    game.style.display = 'flex';
+    startNewGame();
 }
 
+function startNewGame() {
+    isGameActive = true;
+    currentPlayer = 'X';
+    board = ["", "", "", "", "", "", "", "", ""];
+    turnText.textContent = "Player X's Turn";
+    winnerMessage.style.display = 'none';
 
+    for (var i = 0; i < boxes.length; i++) {
+        boxes[i].innerText = '';
+        boxes[i].classList.remove('x', 'o', 'win');
+    }
+}
 
-const checkWinner = () => {
-  for(let pattern of winPatterns){
-    let pos1val = boxes[pattern[0]].innerText;
-     let pos2val = boxes[pattern[1]].innerText;
-      let pos3val = boxes[pattern[2]].innerText;
+function handleBoxClick(event) {
+    var clickedBox = event.target;
+    var index = clickedBox.dataset.index;
 
-      if(pos1val != "" && pos2val !="" && pos3val != ""){
-        if(pos1val === pos2val && pos2val === pos3val){
-          console.log("winner",pos1val)
-          showWinner(pos1val);
+    if (board[index] !== '' || !isGameActive) {
+        return;
+    }
+
+    board[index] = currentPlayer;
+    clickedBox.innerText = currentPlayer;
+    clickedBox.classList.add(currentPlayer.toLowerCase());
+
+    checkResult();
+
+    if (isGameActive) {
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        turnText.textContent = "Player " + currentPlayer + "'s Turn";
+
+        if (gameMode === 'singlePlayer' && currentPlayer === 'O') {
+            turnText.textContent = "Computer's Turn";
+            setTimeout(computerTurn, 500);
         }
-      }
+    }
+}
 
-  }
-} ;
+for (var i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener('click', handleBoxClick);
+}
 
-newGameBtn.addEventListener("click",resetgame);
+function checkResult() {
+    var roundWon = false;
+    for (var i = 0; i < winningConditions.length; i++) {
+        var condition = winningConditions[i];
+        var a = board[condition[0]];
+        var b = board[condition[1]];
+        var c = board[condition[2]];
 
-resetBtn.addEventListener("click",resetgame);
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon) {
+        message.textContent = 'Player ' + currentPlayer + ' Wins!';
+        winnerMessage.style.display = 'flex';
+        isGameActive = false;
+        return;
+    }
+
+    var isDraw = !board.includes("");
+    if (isDraw) {
+        message.textContent = "It's a Draw!";
+        winnerMessage.style.display = 'flex';
+        isGameActive = false;
+    }
+}
+
+function computerTurn() {
+    if (!isGameActive) return;
+
+    var availableSpots = [];
+    for (var i = 0; i < board.length; i++) {
+        if (board[i] === '') {
+            availableSpots.push(i);
+        }
+    }
+
+    var randomIndex = Math.floor(Math.random() * availableSpots.length);
+    var computerMoveIndex = availableSpots[randomIndex];
+
+    board[computerMoveIndex] = 'O';
+    var box = document.querySelector('.box[data-index="' + computerMoveIndex + '"]');
+    box.innerText = 'O';
+    box.classList.add('o');
+
+    checkResult();
+
+    if (isGameActive) {
+        currentPlayer = 'X';
+        turnText.textContent = "Player X's Turn";
+    }
+}
